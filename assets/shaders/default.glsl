1e088239ee010f78da6970e06eac12f77a89447a
SciencePotato <hchan7235@bths.edu>
2024-02-02 15:39:47 -0500

    Shader Added Noise based and Time base Shader

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -4,11 +4,14 @@
 layout (location=0) in vec3 aPos;
 layout (location=1) in vec4 aColor;
 
+uniform mat4 uProj;
+uniform mat4 uView;
+
 out vec4 fColor;
 
 void main() {
     fColor = aColor;
-    gl_Position = vec4(aPos, 1.0);
+    gl_Position = uProj * uView * vec4(aPos, 1.0);
 }
 
 #type fragment
@@ -16,9 +19,16 @@ void main() {
 
 in vec4 fColor;
 
+uniform float uTime;
+
 out vec4 color;
 
 void main() {
-    color = fColor;
+    // Time based
+    //color = sin(uTime) * fColor;
+
+    // Noise
+    float noise = fract(sin(dot(fColor.xy, vec2(12.9898, 78.233))) * 43758.5453);
+    color = fColor * noise;
 }
 
